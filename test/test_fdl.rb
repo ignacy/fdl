527fedb990269f5d8c9eb89a30ad53f0c4d88cf5
Ignacy Moryc <imoryc@gmail.com>
2011-05-29 18:34:16 +0200

    Fixed tests

--- test/test_fdl.rb
+++ test/test_fdl.rb
@@ -8,23 +8,23 @@ class TestFdl < MiniTest::Unit::TestCase
     include Rack::Test::Methods
 
     def app
-        Fdl::Fdl.new
+        Fdl::WebApp.new
     end
 
     def test_post_log_message
-        Fdl::Fdl.expects(:push).with("hello")
+        Fdl::WebApp.expects(:push).with("hello")
         get "/log", message: "hello"
         assert_equal 204, last_response.status
     end
 
     def test_post_message_epty
-       Fdl::Fdl.expects(:push).never 
+       Fdl::WebApp.expects(:push).never 
        get "/log", message: "       "
        assert_equal 400, last_response.status
     end
 
     def test_post_no_message
-        Fdl::Fdl.expects(:push).never
+        Fdl::WebApp.expects(:push).never
         get "/log" 
         assert_equal 400, last_response.status
     end
